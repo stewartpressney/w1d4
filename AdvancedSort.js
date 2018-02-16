6b2ed4139d40fba0c0887c6d6b51dff4eb5358aa
stewartpressney <stewartpressney@gmail.com>
2018-02-15 16:51:18 -0800

    sort excercise

---
+++ AdvancedSort.js
@@ -0,0 +1,21 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort(function(a, b){
+    var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase()
+    var dateA=new Date(a.age), dateB=new Date(b.age)
+    if (nameA < nameB) //sort string ascending
+        return -1
+    if (nameA > nameB)
+        return 1
+    if (nameA === nameB)
+      return dateB-dateA
+    return 0 //default return value (no sorting)
+})
+
+
+console.log(students);
---
+++ sort.js
@@ -0,0 +1,7 @@
+var array = [10, 2, 5, 1, 9];
+  array.sort(function(a,b){ //Array now becomes [7, 8, 25, 41]
+    return b - a
+})
+
+
+console.log(array)
